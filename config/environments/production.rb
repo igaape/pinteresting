78298ab5dee628e293e761a1ac4399b1d860a804
igaape <gaape@live.com>
2014-06-27 17:17:23 -0700

    Devise Implemented (login system)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+    config.action_mailer.default_url_options = { :host => 'rushipint.com' }
+  # Required for Heroku Login
+
 end
