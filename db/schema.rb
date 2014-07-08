8b1cf6ecc1c9e22fc7e98e42569b3810ae92f5a5
igaape <gaape@live.com>
2014-07-08 16:56:05 -0700

    Added Column for Nameas well as created DB for name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140708002933) do
+ActiveRecord::Schema.define(version: 20140708234520) do
 
   create_table "high_scores", force: true do |t|
     t.string   "game"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20140708002933) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
